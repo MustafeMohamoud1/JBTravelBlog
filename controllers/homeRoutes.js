9f0f8b5134c53e2f1cab795a39e6c9822bc3e9b0
BLealan <143086275+BLealan@users.noreply.github.com>
2024-01-15 19:08:22 +0000

    log in details/interaction with db

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+//GET request for travel recommendations and user information
+
+//withAuth?
+
+//Check if user logged in, and brings to profile page
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
