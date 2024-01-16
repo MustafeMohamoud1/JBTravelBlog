86ae9cc4dec2907a874fccb5db03ff1122f3c8d6
lalitakapadia <lalita.kapadia@gmail.com>
2024-01-16 12:09:10 +0000

    working on login-handlebars and signup handlebars using tailwind css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,4 +15,10 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+
+//Check if user logged in, and brings to profile page
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
 module.exports = router;
