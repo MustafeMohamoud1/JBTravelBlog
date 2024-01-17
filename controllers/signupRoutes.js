e09f17823fe2afdb2ae440bb8347b45018e07b1d
lalitakapadia <lalita.kapadia@gmail.com>
2024-01-17 16:28:56 +0000

    login and logout working in  homepage

--- controllers/signupRoutes.js
+++ controllers/signupRoutes.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 
 //Check if user logged in, and brings to profile page
 router.get('/signup', (req, res) => {
-    res.render('signup');
+    if (req.session.loggedIn) {
+        res.redirect('/');     
+    } else {
+        res.render('signup');
+   }
 });
 
 module.exports = router;
