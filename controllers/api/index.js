b1745d120e74f1bc9bf245977da39d9e02e43be9
BLealan <143086275+BLealan@users.noreply.github.com>
2024-01-19 12:19:09 +0000

    deleted stray test files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recommendationRoutes = require('./recommendationRoutes');
-const test = require('./test');
 
 router.use('/users', userRoutes);
 router.use('/recommendation', recommendationRoutes);
-router.use('/test');
 
 module.exports = router;
